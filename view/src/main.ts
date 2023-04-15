8c4be12506e56f7ffd52147f4598a5cb7ef873cd
rramiachraf <51409801+rramiachraf@users.noreply.github.com>
2023-04-15 23:22:45 +0000

    Add keyboard shortcuts

--- view/src/main.ts
+++ view/src/main.ts
@@ -1,9 +1,66 @@
-import "normalize.css"
+import 'normalize.css'
 import '@fontsource/inter/variable.css'
-import App from "./App.svelte"
+import App from './App.svelte'
+import {
+	forward,
+	backward,
+	changeVolume,
+	songVolume,
+	togglePlay,
+	toggleRepeat,
+	toggleShuffle,
+	toggleMute
+} from './components/player/audio'
 
 const app = new App({
-	target: document.body,
+	target: document.body
 })
+
+function getSongVolume() {
+	let volume: number
+	songVolume.subscribe(v => (volume = v))()
+	return volume
+}
+
+function handleKeyboard(e: KeyboardEvent) {
+	switch (e.key) {
+		case ' ':
+			togglePlay()
+			break
+		case 'l':
+			forward()
+			break
+		case 'h':
+			backward()
+			break
+		case 'k':
+			const volumeUp = getSongVolume() + 0.05
+			if (volumeUp < 1) {
+				changeVolume(volumeUp)
+			} else {
+				changeVolume(1)
+			}
+			break
+		case 'j':
+			const volumeDown = getSongVolume() - 0.05
+			if (volumeDown < 0) {
+				changeVolume(0)
+			} else {
+				changeVolume(volumeDown)
+			}
+			break
+		case 'm':
+			toggleMute()
+			break
+		case 'r':
+			toggleRepeat()
+			break
+		case 's':
+			toggleShuffle()
+			break
+	}
+}
+
+window.addEventListener('keydown', handleKeyboard)
 
 export default app
