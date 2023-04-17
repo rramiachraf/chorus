a0fad0c20c31166f216691c71ce8a77194af45a4
rramiachraf <51409801+rramiachraf@users.noreply.github.com>
2023-04-17 18:26:29 +0000

    Move API routes into /api subroutes

--- handlers/server.go
+++ handlers/server.go
@@ -13,16 +13,19 @@ import (
 func StartServer(port int) {
 	r := chi.NewRouter()
 
-	r.Get("/songs", GetSongs)
-	r.Get("/albums", GetAlbums)
-	r.Get("/artists", GetArtists)
-	r.Get("/listen/{id}", ListenToSong)
-	r.Get("/picture/{id}", GetPicture)
-	r.Get("/song/{id}", GetSong)
-	r.Get("/album/{id}", GetAlbum)
-	r.Get("/artist/{id}", GetArtist)
-	r.Get("/random/song", GetRandomSong)
-	r.Get("/stats", GetStats)
+	r.Route("/api", func(r chi.Router) {
+		r.Get("/songs", GetSongs)
+		r.Get("/albums", GetAlbums)
+		r.Get("/artists", GetArtists)
+		r.Get("/listen/{id}", ListenToSong)
+		r.Get("/picture/{id}", GetPicture)
+		r.Get("/song/{id}", GetSong)
+		r.Get("/album/{id}", GetAlbum)
+		r.Get("/artist/{id}", GetArtist)
+		r.Get("/random/song", GetRandomSong)
+		r.Get("/stats", GetStats)
+	})
+
 	r.NotFound(http.HandlerFunc(NotFoundHandler))
 
 	srv := &http.Server{
--- view/vite.config.ts
+++ view/vite.config.ts
@@ -2,15 +2,10 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-	plugins: [
-		svelte({})
-	],
+	plugins: [svelte({})],
 	server: {
 		proxy: {
-			'/api': {
-				target: 'http://localhost:3000',
-				rewrite: path => path.replace(/^\/api/, '')
-			}
+			'/api': 'http://localhost:3000'
 		}
 	}
 })
